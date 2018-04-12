03b3593afad0d7fe8f77d36643165ba1239a451d
Dong <donly@magicalboy.com>
2018-04-12 10:05:26 +0800

    first version

--- 3DTouch/ViewController.swift
+++ 3DTouch/ViewController.swift
@@ -12,14 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        resetBackgroundColor()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func resetBackgroundColor() {
+        view.backgroundColor = UIColor(red: 0,
+                                       green: 0.5,
+                                       blue: 0.5,
+                                       alpha: 100)
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        print("touchesBegan")
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if traitCollection.forceTouchCapability == .available {
+            let touch = touches.first
+            
+            print("\n force:\(touch!.force),maximumPossibleForce:\(touch!.maximumPossibleForce)")
+            
+            view.backgroundColor = UIColor(red: (touch?.force)! / (touch?.maximumPossibleForce)!,
+                                           green: 0.5,
+                                           blue: 0.5,
+                                           alpha: 100)
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        print("touchesCancelled")
+        resetBackgroundColor()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        print("touchesEnded")
+        resetBackgroundColor()
+    }
 }
 
